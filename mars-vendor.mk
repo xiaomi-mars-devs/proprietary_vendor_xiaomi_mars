ebf13f52fc1c2b47bdd72f8ced6503fdc585ac97
FlowerSea0208 <zzp060208@gmail.com>
2024-10-02 04:00:19 +0800

    mars: Switch to source-built fx.tunnel interface
    Change-Id: I1b96a1df3758106b7516ee7b9ac90bd9374f6d1c

--- mars-vendor.mk
+++ mars-vendor.mk
@@ -885,7 +885,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/mars/proprietary/vendor/lib64/vendor.xiaomi.hardware.campostproc@1.0-service-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.xiaomi.hardware.campostproc@1.0-service-impl.so \
     vendor/xiaomi/mars/proprietary/vendor/lib64/vendor.xiaomi.hardware.campostproc@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.xiaomi.hardware.campostproc@1.0.so \
     vendor/xiaomi/mars/proprietary/vendor/lib64/vendor.xiaomi.hardware.dtool@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.xiaomi.hardware.dtool@1.0.so \
-    vendor/xiaomi/mars/proprietary/vendor/lib64/vendor.xiaomi.hardware.fx.tunnel@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.xiaomi.hardware.fx.tunnel@1.0.so \
     vendor/xiaomi/mars/proprietary/vendor/lib64/vl53l5.hal@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vl53l5.hal@1.0.so \
     vendor/xiaomi/mars/proprietary/vendor/lib64/vl53l5.hal@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vl53l5.hal@2.0.so
 
